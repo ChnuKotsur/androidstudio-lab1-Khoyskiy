0019dd5bf485c6a91573b5999a55f3d2c088faee
khokh <vlodarchuk.kseniia@chnu.edu.ua>
2024-03-27 16:49:37 +0200

    done 3

--- app/src/main/java/com/example/lab/ReceiveMessageActivity.java
+++ app/src/main/java/com/example/lab/ReceiveMessageActivity.java
@@ -2,13 +2,22 @@ package com.example.lab;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
 
+import java.util.Locale;
+
 public class ReceiveMessageActivity extends AppCompatActivity {
+    //Количество секунд на секундомере.
+    private int seconds = 0;
+    //Секундомер работает?
+    private boolean running;
+    private boolean wasRunning;
 
     public static final String EXTRA_MESSAGE = "message";
     @Override
@@ -19,6 +28,61 @@ public class ReceiveMessageActivity extends AppCompatActivity {
         String messageText  = intent.getStringExtra(EXTRA_MESSAGE);
         TextView messageView = (TextView) findViewById(R.id.messageT);
         messageView.setText(messageText);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+        runTimer();
 
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+    public void onClickStart(View view) {
+        running = true;
+    }
+    public void onClickStop(View view) {
+        running = false;
+    }
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
